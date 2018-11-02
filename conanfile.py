85609b5cd8f4f35913fec032194a782cc40bd59d
Matt Clarke <matt.clarke@esss.se>
2018-11-02 11:54:23 +0100

    Fixed formatting

--- conanfile.py
+++ conanfile.py
@@ -19,14 +19,20 @@ class DateConan(ConanFile):
     exports_sources = ["CMakeLists.txt", "date-config.cmake"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False], "use_system_tz_db": [True, False], "use_tz_db_in_dot": [True, False], "disable_string_view": [True, False]}
-    default_options = """
-    shared=True
-    fPIC=True
-    use_system_tz_db=True
-    use_tz_db_in_dot=False
-    disable_string_view=False
-    """
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "use_system_tz_db": [True, False],
+        "use_tz_db_in_dot": [True, False],
+        "disable_string_view": [True, False],
+    }
+    default_options = (
+        "shared=True",
+        "fPIC=True",
+        "use_system_tz_db=True",
+        "use_tz_db_in_dot=False",
+        "disable_string_view=False",
+    )
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
@@ -45,12 +51,7 @@ class DateConan(ConanFile):
             self.requires("libcurl/7.56.1@bincrafters/stable")
 
     def source(self):
-        #tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256="98907d243397483bd7ad889bf6c66746db0d7d2a39cc9aacc041834c40b65b98")
-        #extracted_dir = self.name + "-" + self.version
-        #os.rename(extracted_dir, self._source_subfolder)
-        self.run(
-            "git clone https://github.com/HowardHinnant/date.git"
-        )
+        self.run("git clone https://github.com/HowardHinnant/date.git")
         with tools.chdir("./{}".format(self.name)):
             self.run("git checkout 54e8516af223670b75d7a17c2538c6e6d0843c1f .")
             self._source_subfolder = os.getcwd()
@@ -64,10 +65,8 @@ class DateConan(ConanFile):
     def build(self):
         files.mkdir("./{}/build".format(self.name))
         shutil.copyfile(
-            "conanbuildinfo.cmake",
-            "./{}/build/conanbuildinfo.cmake".format(self.name)
+            "conanbuildinfo.cmake", "./{}/build/conanbuildinfo.cmake".format(self.name)
         )
-        print("HELLO" + os.getcwd())
         with tools.chdir("./{}/build".format(self.name)):
             cmake = CMake(self)
             cmake.definitions["ENABLE_DATE_TESTING"] = False
@@ -87,13 +86,19 @@ class DateConan(ConanFile):
         self.copy(pattern="*.a", dst="lib", keep_path=False)
         self.copy(pattern="*.so*", dst="lib", keep_path=False)
         self.copy(pattern="*.dylib", dst="lib", keep_path=False)
-        self.copy(pattern="date-config.cmake", dst="lib/cmake/date-"+self.version, keep_path=False)
+        self.copy(
+            pattern="date-config.cmake",
+            dst="lib/cmake/date-" + self.version,
+            keep_path=False,
+        )
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("pthread")
         use_system_tz_db = 0 if self.options.use_system_tz_db else 1
-        defines = ["USE_AUTOLOAD={}".format(use_system_tz_db),
-                   "HAS_REMOTE_API={}".format(use_system_tz_db)]
+        defines = [
+            "USE_AUTOLOAD={}".format(use_system_tz_db),
+            "HAS_REMOTE_API={}".format(use_system_tz_db),
+        ]
         self.cpp_info.defines.extend(defines)
