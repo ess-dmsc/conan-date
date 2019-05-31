92eacad08977a2fc3613ad7af9560405aea4712a
Matthew D Jones <matthew.d.jones@tessella.com>
2019-05-31 13:45:53 +0100

    Add Windows support

--- conanfile.py
+++ conanfile.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, CMake, tools
+from conans.model.version import Version
+from conans.errors import ConanInvalidConfiguration
 import os
-from conans.util import files
-import shutil
 
 
 class DateConan(ConanFile):
     name = "date"
-    version = "4b46deb"
+    version = "27d1e1e"
     description = "A date and time library based on the C++11/14/17 <chrono> header"
     url = "https://github.com/ess-dmsc/conan-date"
     homepage = "https://github.com/HowardHinnant/date"
@@ -18,72 +18,58 @@ class DateConan(ConanFile):
     exports_sources = ["CMakeLists.txt", "date-config.cmake"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
-        "use_system_tz_db": [True, False],
-        "use_tz_db_in_dot": [True, False],
-        "disable_string_view": [True, False],
-    }
-    default_options = (
-        "shared=True",
-        "fPIC=True",
-        "use_system_tz_db=True",
-        "use_tz_db_in_dot=False",
-        "disable_string_view=False",
-    )
+    options = {"shared": [True, False], "fPIC": [True, False], "use_system_tz_db": [True, False],
+               "use_tz_db_in_dot": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "use_system_tz_db": True, "use_tz_db_in_dot": False}
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
             self.options.remove("fPIC")
-        if self.settings.os == "Macos":
-            self.options["libcurl"].darwin_ssl = False
+            self.options.remove("use_system_tz_db")
+            self.options.remove("use_tz_db_in_dot")
 
     def configure(self):
-        # FIXME: It's not working on Windows
-        if self.settings.os == "Windows":
-            raise Exception("Date is not working on Windows yet.")
+        compiler_version = Version(self.settings.compiler.version.value)
+        if self.settings.compiler == "Visual Studio" and compiler_version < "14":
+            raise ConanInvalidConfiguration("date requires Visual Studio 2015 and higher")
+        if self.settings.compiler == "apple-clang" and compiler_version < "8.0":
+            raise ConanInvalidConfiguration("date requires Apple Clang 8 and higher")
 
     def requirements(self):
-        self.requires("libcurl/7.56.1@bincrafters/stable")
-        self.requires("OpenSSL/1.0.2n@conan/stable")
-        self.requires("zlib/1.2.11@conan/stable")
+        if self.settings.os == "Windows" or not self.options.use_system_tz_db:
+            self.requires("libcurl/7.56.1@bincrafters/stable")
 
     def source(self):
-        self.run("git clone https://github.com/HowardHinnant/date.git")
+        self.run("git clone https://github.com/HowardHinnant/date.git " + self._source_subfolder)
 
         with tools.chdir(os.path.join(self.source_folder, self.name)):
-            self.run("git checkout 54e8516af223670b75d7a17c2538c6e6d0843c1f .")
+            self.run("git checkout " + self.version + " .")
+            
+        tools.replace_in_file(os.path.join(self._source_subfolder, 'CMakeLists.txt'),
+                              '${CURL_LIBRARIES}', '${CONAN_LIBS}')
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.configure(source_dir=self.name, build_dir=".")
+        cmake.definitions["ENABLE_DATE_TESTING"] = False
+        if self.settings.os == "Windows":
+            cmake.definitions["USE_TZ_DB_IN_DOT"] = False
+            cmake.definitions["USE_SYSTEM_TZ_DB"] = False
+        else:
+            cmake.definitions["USE_TZ_DB_IN_DOT"] = self.options.use_tz_db_in_dot
+            cmake.definitions["USE_SYSTEM_TZ_DB"] = self.options.use_system_tz_db
+        cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        files.mkdir("./{}/build".format(self.name))
-        shutil.copyfile(
-            "conanbuildinfo.cmake", "./{}/build/conanbuildinfo.cmake".format(self.name)
-        )
-        with tools.chdir("./{}/build".format(self.name)):
-            cmake = CMake(self)
-            cmake.definitions["ENABLE_DATE_TESTING"] = False
-            cmake.definitions["USE_SYSTEM_TZ_DB"] = self.options.use_system_tz_db
-            cmake.definitions["USE_TZ_DB_IN_DOT"] = self.options.use_tz_db_in_dot
-            cmake.definitions["DISABLE_STRING_VIEW"] = self.options.disable_string_view
-            cmake.configure(source_dir="..", build_dir=".")
-            cmake.build(build_dir=".")
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
-        src_folder = os.path.join(self.source_folder, self.name)
-        self.copy(pattern="LICENSE*", dst="licenses", src=src_folder)
-        include_folder = os.path.join(src_folder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
         self.copy(
             pattern="date-config.cmake",
             dst="lib/cmake/date-" + self.version,
@@ -94,9 +80,10 @@ class DateConan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("pthread")
-        use_system_tz_db = 0 if self.options.use_system_tz_db else 1
-        defines = [
-            "USE_AUTOLOAD={}".format(use_system_tz_db),
-            "HAS_REMOTE_API={}".format(use_system_tz_db),
-        ]
+        if self.settings.os == "Windows":
+            use_system_tz_db = 0
+        else:
+            use_system_tz_db = 0 if self.options.use_system_tz_db else 1
+        defines = ["USE_AUTOLOAD={}".format(use_system_tz_db),
+                   "HAS_REMOTE_API={}".format(use_system_tz_db)]
         self.cpp_info.defines.extend(defines)
